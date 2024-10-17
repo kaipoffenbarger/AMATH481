56924214cc30f5710845b6dd94a601ebd178d600
kaipoffenbarger <kaipoff@uw.edu>
2024-10-17 09:58:41 -0700

    Add files via upload

---
+++ hw2.py
@@ -0,0 +1,47 @@
+import numpy as np
+from scipy.integrate import odeint
+import matplotlib.pyplot as plt
+
+def shoot2(y, x, n0, epsilon):
+    return [y[1], (x**2 - epsilon) * y[0]]
+
+tol = 1e-4  # define a tolerance level 
+epsilon_start = 0.5  # beginning value of epsilon
+L = 4
+col = ['r', 'b', 'g', 'c', 'm', 'k']  # eigenfunc colors
+n0 = 16; A = 1; phi0 = [1, np.sqrt(L**2 - epsilon_start)]; xp = [-L, L] 
+xshoot =  np.arange(xp[0], xp[1]+0.1, 0.1)
+
+A1 = np.zeros((len(xshoot), 5)) # for eigenfunctions values
+A2 = np.array([]) # for eigenvalues
+
+
+for modes in range(1, 6):  # begin mode loop
+    epsilon = epsilon_start  # initial value of eigenvalue epsilon
+    depsilon = n0 / 100  # default step size in epsilon
+    for _ in range(1000):  # begin convergence loop for epsilon
+        y = odeint(shoot2, phi0, xshoot, args=(n0, epsilon))  
+
+        if abs(y[-1, 0] - 0) < tol:  # check for convergence
+            print(epsilon)  # write out eigenvalue
+            A2 = np.append(A2, epsilon) # add eigenvalue before breaking out
+            break  # get out of convergence loop
+
+        if (-1) ** (modes + 1) * y[-1, 0] > 0:
+            epsilon += depsilon
+        else:
+            epsilon -= depsilon / 2
+            depsilon /= 2
+
+    epsilon_start = epsilon + 0.5  # after finding eigenvalue, pick new start
+    norm = np.trapz(y[:, 0] * y[:, 0], xshoot)  # calculate the normalization
+    eigenfunc_normalized = y[:, 0] / np.sqrt(norm) # get normalized eigenfunc values
+    A1[:, modes - 1] = np.abs(eigenfunc_normalized) # add them to matrix
+
+    plt.plot(xshoot, y[:, 0] / np.sqrt(norm), col[modes - 1], label=f'Eig func {modes}')  # plot modes
+    
+plt.legend(loc='lower left')
+plt.show()  # end mode loop
+
+print(A1)
+print(A2)
